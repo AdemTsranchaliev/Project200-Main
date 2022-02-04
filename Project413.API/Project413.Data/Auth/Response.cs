b046a284d40196cd7061e476e08150bbc25a9a36
Adem <adem@adem>
2022-02-04 12:28:13 +0200

    Added auth

---
+++ Project413.API/Project413.Data/Auth/Response.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Project413.Data.Auth
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
+
