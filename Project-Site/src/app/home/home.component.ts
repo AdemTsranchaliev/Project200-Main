8c978223ac2c6e14f737f32d5b9b22da77d7a4c6
Stoiko Tarkalanov <76123758+Stoyko-Tarkalanov@users.noreply.github.com>
2023-06-19 22:01:51 +0300

    Adding library for Swiper

--- Project-Site/src/app/home/home.component.ts
+++ Project-Site/src/app/home/home.component.ts
@@ -1,16 +1,19 @@
 // Angular
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 // RXJS
 import { Subscription } from 'rxjs';
 // Services
 import { CatalogService } from '../shared/services/catalog.service';
+// Test Swiper Librari
+import Swiper from 'swiper';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   // Subscriptions
   private subscriptions: Subscription[] = [];
 
@@ -29,6 +32,33 @@ export class HomeComponent implements OnInit {
   ngOnDestroy() {
     this.subscriptions.forEach(el => el.unsubscribe());
   }
+
+  // Test Swiper
+  ngAfterViewInit() {
+    new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      loop: true,
+      roundLengths: true,
+      breakpoints: {
+        1200: {
+          slidesPerView: 4
+        },
+        991: {
+          slidesPerView: 2
+        },
+        565: {
+          slidesPerView: 1
+        }
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+      }
+    });
+  }
+
 
   /**
    * This method create request to get all Salons for Home Page Slider
