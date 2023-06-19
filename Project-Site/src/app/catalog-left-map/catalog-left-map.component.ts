376b068917e2280f2af49626f76252859ca460e3
Stoiko Tarkalanov <76123758+Stoyko-Tarkalanov@users.noreply.github.com>
2023-06-19 21:37:08 +0300

    Adding map to Catalog Page

--- Project-Site/src/app/catalog-left-map/catalog-left-map.component.ts
+++ Project-Site/src/app/catalog-left-map/catalog-left-map.component.ts
@@ -1,5 +1,5 @@
 // Angular
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, EventEmitter, Input, Output } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
 // RXJS
@@ -9,6 +9,11 @@ import { CatalogService } from '../shared/services/catalog.service';
 // Components
 import { PaginatorComponent } from '../shared/components/paginator/paginator.component';
 import { CatalogFilterModalComponent } from '../shared/components/modals/catalog-filter-modal/catalog-filter-modal.component';
+
+// ================= Test Map | Will be removed =================
+import * as L from 'leaflet';
+import { Coordinates } from '../shared/models/studio/coordinates.model';
+// ================= Test Map | Will be removed =================
 
 @Component({
   selector: 'app-catalog-left-map',
@@ -29,6 +34,18 @@ export class CatalogLeftMapComponent implements OnInit {
   catalogData: any; // Salon Data
   catalogOptionsData: any; // All Filters & ect. Data
 
+
+  // ================= Test Map | Will be removed =================
+  @Input() coordinates: Coordinates;
+  @Output() getLocationEvent = new EventEmitter<Coordinates>();
+
+  private map: L.Map;
+  private marker: any;
+
+  private defaultCoordinates: L.LatLngExpression = [42.588612, 25.301165];
+  private zoom: number = 7;
+  // ================= Test Map | Will be removed =================
+
   constructor(
     private catalogService: CatalogService,
     public dialog: MatDialog
@@ -39,6 +56,9 @@ export class CatalogLeftMapComponent implements OnInit {
     this.getSalonData();
     // Get All Filter Options
     this.getSalonFilterOptionsData();
+
+    // ================= Test Map | Will be removed =================
+    this.initMap();
   }
 
   ngOnDestroy() {
@@ -92,5 +112,57 @@ export class CatalogLeftMapComponent implements OnInit {
     // ============== Check what this do ==============
     const dialogSubscription = dialogRef.afterClosed().subscribe();
     this.subscriptions.push(dialogSubscription);
+  }
+
+
+
+  // ================= Test Map | Will be removed =================
+
+  private initMap() {
+    this.map = L.map('map').setView(this.defaultCoordinates, this.zoom);
+
+    this.addMarker();
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(this.map);
+
+    if (this.coordinates) {
+      this.addMarkerByCoordinates(this.coordinates);
+    }
+  }
+
+  ngOnChanges(changes: any) {
+    if (
+      changes.coordinates != null &&
+      changes.coordinates.currentValue != null
+    ) {
+      this.addMarkerByCoordinates(this.coordinates);
+    }
+  }
+
+  private addMarker() {
+    this.map.addEventListener('click', (x) => {
+      if (this.marker) {
+        this.map.removeLayer(this.marker);
+      }
+      this.marker = L.marker([x.latlng.lat, x.latlng.lng]);
+      this.marker.addTo(this.map);
+
+      var map: Coordinates = { latitude: x.latlng.lat, longitude: x.latlng.lng };
+      this.getLocationEvent.next(map);
+    });
+  }
+
+  private addMarkerByCoordinates(coordinates: any) {
+    if (this.marker) {
+      this.map.removeLayer(this.marker);
+    }
+    this.marker = L.marker([coordinates.lat, coordinates.lng]);
+    this.marker.addTo(this.map);
+
+    var map: Coordinates = { latitude: coordinates.lat, longitude: coordinates.lng };
+    this.getLocationEvent.next(map);
   }
 }
